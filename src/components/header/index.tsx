60bdc72f6f75df93c11bd4bdc8b5ccb8e28afd9b
William Araujo <william.araujo@kumulus.com.br>
2021-12-14 17:17:13 -0300

    first commit

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import * as S from './styles'
 
 export default function DefaultHeader() {
+  const data = 'Header'
   return (
     <S.ContainerHeader>
-      <h3>Header</h3>
+      <h3>{data}</h3>
     </S.ContainerHeader>
   )
 }
